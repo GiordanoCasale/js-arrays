256af465bc7abbf03f13e744b80a624ad6dde038
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-19 15:58:47 +0100

    [UPD] First bonus completed

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,23 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+for(let i=teachers.length - 1; i>=0; i--){
+  reversedTeachers.push(teachers[i])
+}
+console.log(reversedTeachers)
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = null;
+for(let i=0; i<teachers.length; i++){
+  
+}
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
