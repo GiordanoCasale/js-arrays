2e53a480fc2d206ea1f78e301cee5cfd6e051fa2
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-19 18:11:30 +0100

    [UPD] Third, fourth and fifth bonus completed

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -26,11 +26,30 @@ for(let i=0; i<teachers.length; i++){
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1,1)
+console.log(teachers)
 
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let flag = false
+for(let i = 0; i<teachers.length; i++){
+  if(teachers[i] != ""){
+    flag = true
+  }
+}
+const isFabioPresent = flag;
+console.log(isFabioPresent)
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let strings = "";
+for(let i = 0; i<teachers.length; i++){
+  strings+= teachers[i];
+  if(i<teachers.length - 1){
+    strings+=", ";
+  }
+}
+
+const teachersString = strings;
+console.log(teachersString)
